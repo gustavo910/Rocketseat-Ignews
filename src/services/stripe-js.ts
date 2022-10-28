76d6a2c66d3f3152a638ceafc760f8d0f759baf1
gustavo.bastos@pti.org.br <gustavo.bastos@pti.org.br>
2022-10-28 15:24:50 -0300

    Final

--- src/services/stripe-js.ts
+++ src/services/stripe-js.ts
@@ -1,7 +1,7 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export async function getStripeJs() {
-    const publishableKey = process.env.STRIPE_PUBLIC_KEY;
+    const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
     const stripeJs =await loadStripe(publishableKey);
     return stripeJs;
 }
